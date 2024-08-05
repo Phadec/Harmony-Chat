4399ec49afcfe805fe09d16e7cc293c59f012156
Phadec <21130467@st.hcmuaf.edu.vn>
2024-08-05 20:26:05 +0700

    update

--- ChatAppServer/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
+++ ChatAppServer/ChatAppServer.WebAPI/obj/Debug/net8.0/ChatAppServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf4e39fa6dc655b7ca674d8cb45f3ac8e467cc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c158fd89bd8bd6e25404e4c61f2da72abfbb49bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatAppServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
