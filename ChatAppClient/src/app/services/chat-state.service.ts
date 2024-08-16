8ae40d4e0039b7fa6fdbe213584b72683a20f9ee
Phadec <21130467@st.hcmuaf.edu.vn>
2024-08-16 21:31:08 +0700

    temp fix

--- ChatAppClient/src/app/services/chat-state.service.ts
+++ ChatAppClient/src/app/services/chat-state.service.ts
@@ -5,12 +5,36 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ChatStateService {
-  private recipientIdSource = new BehaviorSubject<string | null>(null);
-  currentRecipientId$ = this.recipientIdSource.asObservable();
 
-  constructor() {}
+  private recipientInfoSource = new BehaviorSubject<any>(null);
+  recipientInfo$ = this.recipientInfoSource.asObservable();
 
-  changeRecipientId(recipientId: string) {
-    this.recipientIdSource.next(recipientId);
+  private selectedRecipientIdSubject = new BehaviorSubject<string | null>(null);
+  selectedRecipientId$ = this.selectedRecipientIdSubject.asObservable();
+
+  private messagesSubject = new BehaviorSubject<any[]>([]);
+  messages$ = this.messagesSubject.asObservable();
+
+  updateRecipientInfo(info: any) {
+    if (info) {
+      console.log('ChatStateService - Updating recipient info:', info);
+      this.recipientInfoSource.next(info);
+    } else {
+      this.resetChatState();
+    }
+  }
+
+  updateMessages(messages: any[]): void {
+    console.log('ChatStateService - Updating messages:', messages);
+    this.messagesSubject.next(messages);
+  }
+
+  resetChatState(): void {
+    console.log("ChatStateService - Resetting chat state");
+    this.recipientInfoSource.next(null);
+    this.messagesSubject.next([]);
+  }
+  setSelectedRecipient(recipientId: string): void {
+    this.selectedRecipientIdSubject.next(recipientId);
   }
 }
