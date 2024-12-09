c701be0f60f874f17cc497129f806e9e2843e612
Phadec <129315139+Phadec@users.noreply.github.com>
2024-12-10 01:36:13 +0700

    delete old project

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import Login from "~/pages/login/Login";
-import React from "react";
-import Forget from '~/pages/register/Register'
-import Register from "~/pages/register/Register";
-export default function App() {
-   return <Register />
-}
--- src/assets/imgs/Images.tsx
+++
@@ -1,5 +0,0 @@
-import BackgroundLogin from './background-login.png'
-import Facebook from './facebook.png'
-import Google from './google.png'
-import Zalo from './zalo.png'
-export { BackgroundLogin, Facebook, Google, Zalo }
